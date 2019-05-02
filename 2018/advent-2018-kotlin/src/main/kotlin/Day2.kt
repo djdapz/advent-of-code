d5b268488e751be23e166cd71b94bae4a19cb77e
djdapz <djdapz@aol.com>
2019-05-02 00:59:17 -0600

    2018: 2 pt 2 (bruteforce)

--- 2018/advent-2018-kotlin/src/main/kotlin/Day2.kt
+++ 2018/advent-2018-kotlin/src/main/kotlin/Day2.kt
@@ -14,7 +14,7 @@ data class Checksum(val numberOfDoubles: Int, val numberOfTriples: Int) {
         if (s.hasExactlyTwoLetters()) this.numberOfDoubles + 1 else this.numberOfDoubles
 }
 
-fun HashMap<Char, Int>.addOrIncrement(c: Char): HashMap<Char, Int> {
+private fun HashMap<Char, Int>.addOrIncrement(c: Char): HashMap<Char, Int> {
     this[c] = this[c]?.plus(1) ?: 1
     return this
 }
@@ -22,10 +22,50 @@ fun HashMap<Char, Int>.addOrIncrement(c: Char): HashMap<Char, Int> {
 private fun String.toMapOfLetterCount(): HashMap<Char, Int> =
     this.sorted().fold(HashMap(), HashMap<Char, Int>::addOrIncrement)
 
-fun String.hasExactlyTwoLetters(): Boolean = this.toMapOfLetterCount().values.contains(2)
-fun String.hasExactlyThreeLetters(): Boolean = this.toMapOfLetterCount().values.contains(3)
+internal fun String.hasExactlyTwoLetters(): Boolean = this.toMapOfLetterCount().values.contains(2)
+internal fun String.hasExactlyThreeLetters(): Boolean = this.toMapOfLetterCount().values.contains(3)
 
-
-internal fun String.sorted(): String {
+fun String.sorted(): String {
     return toCharArray().sorted().joinToString("")
+}
+
+fun String.findSimmilarities(other: String) = this.toCharArray()
+    .zip(other.toCharArray())
+    .fold("") { similarities, pair ->
+        if (pair.first == pair.second) similarities + pair.first else similarities
+    }
+
+fun isOffByOne(s: String, s1: String): Boolean {
+    return s.findSimmilarities(s1).length == s.length - 1
+}
+
+fun findSimilaritiesOfFirstOffByOne(list: List<String>): Any {
+    for (string1 in list) {
+        for (string2 in list) {
+            val similarities = string1.findSimmilarities(string2)
+            if (similarities.length == string1.length - 1) {
+                return similarities
+            }
+        }
+    }
+    throw Exception("Nothing Found")
+}
+
+
+class Day2(private val input: List<String>) {
+    fun part1() {
+        val checksum = Checksum.fromList(input).checksum
+        println("Part 1: Checksum = $checksum")
+    }
+
+    fun part2() {
+        val simmilarities = findSimilaritiesOfFirstOffByOne(input)
+        println("Part 2: Checksum = $simmilarities")
+    }
+}
+
+
+fun main() {
+    Day2(day2Input).part1()
+    Day2(day2Input).part2()
 }
