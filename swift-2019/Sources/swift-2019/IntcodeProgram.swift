3e5aa49ec89c76166e75cbcb4dcdece814082a98
devon <ddapuzzo@pivotal.io>
2020-09-25 23:45:17 -0500

    refactor based on updated domain terms

--- swift-2019/Sources/swift-2019/IntcodeProgram.swift
+++ swift-2019/Sources/swift-2019/IntcodeProgram.swift
@@ -8,7 +8,7 @@ typealias IntcodeProgram = [Int]
 
 extension IntcodeProgram {
     func runAsIntcode() -> IntcodeProgram {
-        self.processFromWord(at: 0)
+        self.processInstructions(startingAt: 0)
     }
 }
 
@@ -32,40 +32,40 @@ enum Operand {
 }
 
 private extension IntcodeProgram {
-    func firstArgumentFor(word: Int) -> Int {
-        self[self[word * 4 + 1]]
+    func firstParameterFor(instructionPointer: Int) -> Int {
+        self[self[instructionPointer + 1]]
     }
 
-    func secondArgumentFor(word: Int) -> Int {
-        self[self[word * 4 + 2]]
+    func secondParameterFor(instructionPointer: Int) -> Int {
+        self[self[instructionPointer  + 2]]
     }
 
-    func operandFor(word: Int) -> Operand {
-        Operand.from(self[word * 4])
+    func operandFor(instructionPointer: Int) -> Operand {
+        Operand.from(self[instructionPointer])
     }
 
-    func pivotFor(word: Int) -> Int {
-        self[word * 4 + 3]
+    func pivotFor(instructionPointer: Int) -> Int {
+        self[instructionPointer + 3]
     }
 
-    func processFromWord(at wordNumber: Int) -> [Int] {
-        guard let operation = getOperation(forWord: wordNumber) else{
+    func processInstructions(startingAt pointer: Int) -> [Int] {
+        guard let operation = getOperation(from: pointer) else{
             return self
         }
-        let firstArgument = firstArgumentFor(word: wordNumber)
-        let secondArgument = secondArgumentFor(word: wordNumber)
-        let pivot = pivotFor(word: wordNumber)
+        let firstParameter = firstParameterFor(instructionPointer: pointer)
+        let secondParameter = secondParameterFor(instructionPointer: pointer)
+        let pivot = pivotFor(instructionPointer: pointer)
 
-        let sum = operation(firstArgument, secondArgument)
+        let sum = operation(firstParameter, secondParameter)
 
         let processed = insertAt(pivot: pivot, element: sum)
 
-        return processed.processFromWord(at: wordNumber + 1)
+        return processed.processInstructions(startingAt: pointer + 4)
     }
 
 
-    func getOperation(forWord wordNumber: Int) -> ((Int, Int) -> Int)? {
-        let operand = operandFor(word: wordNumber)
+    func getOperation(from pointer: Int) -> ((Int, Int) -> Int)? {
+        let operand = operandFor(instructionPointer: pointer)
         switch operand {
         case .add:
             return { a, b in a + b }
