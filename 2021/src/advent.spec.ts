d058c1834f730febdc20504e34ccfd332c9bebd7
djdapz <djdapz@aol.com>
2021-12-01 00:50:37 -0500

    2021 - refactor run command

--- 2021/src/advent.spec.ts
+++
@@ -1,9 +0,0 @@
-import { input, part1, part2 } from './day1';
-import { readNumberList } from './read';
-
-describe("do it", () => {
-  it('should do it', () => {
-    console.log(part1(readNumberList(input)));
-    console.log(part2(readNumberList(input)));
-  });
-})
---
+++ 2021/src/main.ts
@@ -0,0 +1,8 @@
+import { input, part1, part2 } from './day1';
+import { readNumberList } from './read';
+
+console.log('Day1');
+console.log('part1');
+console.log(part1(readNumberList(input)));
+console.log('part2');
+console.log(part2(readNumberList(input)));
